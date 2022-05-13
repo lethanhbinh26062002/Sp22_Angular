e5b860f9698bab96a6906a0f8bb9a5ea1c57d1c4
Lê Thanh Bình <lethanhbinh26062002@gmail.com>
2022-05-13 11:13:30 +0700

    event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular_SP22';
   name = 'Binhltph16803';
-  id = 'PH16803'
+  id = 'PH16803';
   teachers = [
     {
       id: 1,
@@ -33,6 +33,28 @@ export class AppComponent {
       gender: 1,
       avatar: "https://cdn.sforum.vn/sforum/wp-content/uploads/2018/11/3-8.png",
       status:1
+    },
+    {
+      id: 4,
+      name: "Dung",
+      age: 26,
+      gender: 1,
+      avatar: "https://cdn.sforum.vn/sforum/wp-content/uploads/2018/11/3-8.png",
+      status:0
     }
   ]
+  // Định nghĩa hàm
+  schoolName = "";
+  clickH1(){
+    console.log("Đã click");
+    this.schoolName = 'Poly';
+  }
+  status = true;
+  changeStatus(){
+    this.status = !this.status
+  }
+  valueInput = '';
+  changeInput(event: any){
+    this.valueInput = event.target.value
+  }
 }
